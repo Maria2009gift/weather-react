c91e7113ba17cb2abf8291b969a95e2a719ce62a
Maria2009gift <kovalenkom2009@gmail.com>
2025-01-28 23:50:12 +0200

    form html

---
+++ src/components/FormSignUp/FormSignUp.jsx
@@ -0,0 +1,29 @@
+
+import style from './FormSignUp.module.css'
+
+function FormSignUp() {
+
+    return (
+        <>
+            <h2 className={style.title_signup}>Sign Up</h2>
+            <form action="">
+                <label className={style.container_input}>
+                    <p className={style.minititle_signup}>Username</p>
+                    <input type="text" className={style.input_signup} placeholder='Username'/>
+                </label>
+                <label className={style.container_input}>
+                    <p className={style.minititle_signup}>E-Mail</p>
+                    <input type="text" className={style.input_signup} placeholder='E-Mail'/>
+                </label>
+                <label className={style.container_input}>
+                    <p className={style.minititle_signup}>Password</p>
+                    <input type="text" className={style.input_signup} placeholder='Password'/>
+                </label>
+                <button type="submit">Sign Up</button>
+            </form>
+        </>
+    )
+
+}
+
+export default FormSignUp
